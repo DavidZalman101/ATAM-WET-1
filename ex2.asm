bc9052a76ec5685f6caddca6ee3f7ee7b4fed132
DavidZalman101 <davidzalman.101@gmail.com>
2022-11-25 15:41:11 +0200

    Add files via upload

--- ex2.asm
+++ ex2.asm
@@ -22,21 +22,32 @@ _start:
 # 
 	movl (num), %ecx 		# %rcx = num
 	movq $source, %rbx		# %rbx = &source
+
 	test %ecx, %ecx		        # cheking if num is non-negative	
 	js _NEGATIVE_HW1
 	jmp _NON_NEGATIVE_HW1
 
-_NON_NEGATIVE_HW1:
-	movq $destination, %rax		# %rax = &destination
-	movw $0x01, %dx 		# %rdx = +1
-	jmp _FUNC_HW1
+BACK_HW1:
+	test %dx, %dx
+	je _NEGATIVE_CONT_HW1
+	jmp _NON_NEGATIVE_CONT_HW1
 
 _NEGATIVE_HW1:
+	movw $0x00, %dx 		# %rdx = 0
 	movq $destination, %rax		 
 	xor $0xffffffff, %ecx
-	lea (%rax, %rcx, 1), %rax  	# %rax = &destination + |num|
 	inc %ecx
-	movw $-0x01, %dx 		# %rdx = -1
+	jmp INSERT_DATA_TO_STACK_HW1
+_NEGATIVE_CONT_HW1:
+	movq $destination, %rax		# %rax = &destination
+	jmp _FUNC_HW1
+
+_NON_NEGATIVE_HW1:
+	movq $destination, %rax		 
+	movw $0x01, %dx 		# %rdx = 1
+	jmp INSERT_DATA_TO_STACK_HW1
+_NON_NEGATIVE_CONT_HW1:
+	lea -1(%rax, %rcx, 1), %rax  	# %rax = &destination + |num|
 	jmp _FUNC_HW1
 
 _INC_HW1:
@@ -51,15 +62,34 @@ _FUNC_HW1:
 	test %ecx, %ecx			# checking if we copied all
 	je _EXIT_HW1	
 	
-	movb (%rbx), %r8b
-	movb %r8b, (%rax)		# *(%rax) = *(%rbx)
-	inc %rbx			#  %rbx ++
+
+	popq %r8
+	movb %r8b, (%rax)
 	test %dx, %dx
-	js _DEC_HW1
-	jmp _INC_HW1
+	je _INC_HW1
+	jmp _DEC_HW1
+
 _CONT_HW1:
 	dec %ecx
 	jmp _FUNC_HW1 
-
+					# push 
+INSERT_DATA_TO_STACK_HW1:
+	movq %rcx, %r11			# r11 = counter = num
+	test %r11, %r11
+	je _EXIT_HW1			# counter = 0 -> there is nothing to copy 
+ITERETE_HW1:
+	movb (%rbx), %r10b		# r10 = *(%rbx)
+	inc %rbx			# rbx++
+	push %r10			# store in the stack
+	dec %r11
+	test %r11, %r11			# check if there is more to push to the stack
+	je _CHECK_WHERE_TO_RETURN_HW1
+	jmp ITERETE_HW1
+	
+_CHECK_WHERE_TO_RETURN_HW1:
+	test %dx, %dx
+	je _NEGATIVE_CONT_HW1
+	jmp _NON_NEGATIVE_CONT_HW1
+	
 _EXIT_HW1:
 
